5bdffc0e82515fb994a799906684b4232269ce68
EEDDRV <ethandureus1@gmail.com>
2021-06-21 23:21:10 -0400

    Changes

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 echo "[*] Starting"
 sudo apt update -y
-sudo apt install gdm3 gnome-session gnome-icon-theme --no-install-recommaends -y
+sudo apt install gdm3 gnome-session gnome-icon-theme --no-install-recommends -y
 sudo apt install xserver-xorg -y
 sudo apt install wayland-protocols weston xwayland -y
 sudo apt install gnome-control-center software-properties-gtk gnome-backgrounds gnome-terminal firefox chromium-browser nautilus pcmanfm gnome-tweaks gnome-software -y
