9eac188cb8f42fdce7204d83aafd92f21c2ebb35
EEDDRV <ethandureus1@gmail.com>
2021-06-22 09:44:45 -0400

    Update After-reboot.sh

--- After-reboot.sh
+++ After-reboot.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get install python3-pip -y
 sudo add-apt-repository ppa:camel-neeraj/sysmontask
 sudo apt install sysmontask -y
-sudo pip3 install -U psutil
+sudo pip3 -H install -U psutil
 sudo apt install cairo-dock -y
 echo "Done!"
