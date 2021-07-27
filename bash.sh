ac6362baa608ceb872d51396ee0bd6be35b5c105
EEDDRV <ethandureus1@gmail.com>
2021-07-27 12:20:32 -0400

    Added Vim and g++

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt install gdm3 gnome-session gnome-icon-theme --no-install-recommends -y
 sudo apt install xserver-xorg -y
 sudo apt install wayland-protocols weston xwayland -y
-sudo apt install gnome-control-center software-properties-gtk gnome-backgrounds gnome-terminal firefox chromium-browser nautilus pcmanfm gnome-tweaks gnome-software -y
+sudo apt install gnome-control-center software-properties-gtk gnome-backgrounds gnome-terminal firefox chromium-browser vim g++ nautilus pcmanfm gnome-tweaks gnome-software -y
 sudo apt install git gnome-shell-extensions chrome-gnome-shell gnome-tweak-tool -y
 sudo apt install curl -y
 sudo apt update -y
