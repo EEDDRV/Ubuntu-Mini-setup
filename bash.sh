eb1e7b099611330fff7c8805961a7d051b4cd45d
EEDDRV <ethandureus1@gmail.com>
2022-03-15 19:52:01 -0400

    Added: net-tools

--- bash.sh
+++ bash.sh
@@ -7,5 +7,6 @@ sudo apt install wayland-protocols weston xwayland -y
 sudo apt install gnome-control-center software-properties-gtk gnome-backgrounds gnome-terminal firefox chromium-browser vim g++ nautilus pcmanfm gnome-tweaks gnome-software -y
 sudo apt install git gnome-shell-extensions chrome-gnome-shell gnome-tweak-tool -y
 sudo apt install curl -y
+sudo apt install net-tools -y
 sudo apt update -y
 echo "Done!"
